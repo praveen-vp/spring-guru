6326b752767af438504fd4a7fdcfca5892abb71f
praveen-vp <praveen-vp@localhost>
2018-06-13 23:18:14 +0530

    Adding publisher entinty

---
+++ spring5webapp/src/main/java/com/pvp/spring5webapp/model/Publisher.java
@@ -0,0 +1,44 @@
+package com.pvp.spring5webapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+/**
+ * 
+ * @author praveen-vp
+ * 13-06-2018
+ *
+ */
+
+@Entity
+public class Publisher {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public Publisher(Long id, String name, String address) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+	
+	
+	
+}
