44000812bfaa42269c2cab99becaa2953e08454c
praveen-vp <praveen-vp@localhost>
2018-06-13 23:19:08 +0530

    Adding changes in Book Entity for including Publisher Entity

--- spring5webapp/src/main/java/com/pvp/spring5webapp/model/Book.java
+++ spring5webapp/src/main/java/com/pvp/spring5webapp/model/Book.java
@@ -13,7 +13,9 @@ public class Book {
 
 	private String title;
 	private String isbn;
-	private String publisher;
+	
+	@OneToOne
+	private Publisher publisher;
 
 	@ManyToMany
 	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
@@ -39,23 +41,24 @@ public class Book {
 		this.authors = authors;
 	}
 
-	public Book(String title, String isbn, String publisher) {
-		super();
-		this.title = title;
-		this.isbn = isbn;
-		this.publisher = publisher;
-	}
-
-	public Book(String title, String isbn, String publisher, Set<Author> authos) {
-		super();
-		this.title = title;
-		this.isbn = isbn;
-		this.publisher = publisher;
-		this.authors = authos;
-	}
-
 	public String getTitle() {
 		return title;
+	}
+
+	public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
+
+		this.title = title;
+		this.isbn = isbn;
+		this.publisher = publisher;
+		this.authors = authors;
+	}
+
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
 	}
 
 	public void setTitle(String title) {
@@ -68,14 +71,6 @@ public class Book {
 
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
-	}
-
-	public String getPublisher() {
-		return publisher;
-	}
-
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
 	}
 
 	public Set<Author> getAuthos() {
